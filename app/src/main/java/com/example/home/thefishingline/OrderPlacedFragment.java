17d21d90fc009aa6a2f3ec6a0382dfa35fb92c2f
rsorotzkin <rsorotzkin@gmail.com>
2016-06-19 22:24:57 -0400

    updated checkoutFragment and added orderPlacedFragment

--- app/src/main/java/com/example/home/thefishingline/OrderPlacedFragment.java
+++ app/src/main/java/com/example/home/thefishingline/OrderPlacedFragment.java
@@ -4,35 +4,23 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
-import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
-public class CheckoutFragment extends Fragment {
+public class OrderPlacedFragment extends Fragment {
 
 
 
 
     // Declare activities
     MainActivity mainActivity;
-    CoordinatorLayout checkoutCoordinatorLayout;
 
     @Override
     public void onResume() {
         super.onResume();
-        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Checkout");
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Confirmation");
         ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         MainActivity.toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,7 +40,7 @@ public class CheckoutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_checkout, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_order_placed, container, false);
 
         // Initialize the views for this fragment
         initializeViews(rootView);
@@ -65,7 +53,6 @@ public class CheckoutFragment extends Fragment {
      * Function to initialize controls
      */
     public void initializeViews(final View rootView) {
-        checkoutCoordinatorLayout = (CoordinatorLayout)rootView.findViewById(R.id.checkoutCoordinatorLayout);
 
     }
 
@@ -82,7 +69,6 @@ public class CheckoutFragment extends Fragment {
      */
     public void registerListeners() {
         // set onClickListeners
-        checkoutCoordinatorLayout.setOnClickListener(checkoutCoordinatorLayoutListener);
     }
 
     /**
@@ -92,7 +78,7 @@ public class CheckoutFragment extends Fragment {
 
         @Override
         public void onClick(View v) {
-            Util.replaceFragment(new OrderPlacedFragment(),R.string.app_name);
+            //Util.replaceFragment(new );
 
         }
     };
